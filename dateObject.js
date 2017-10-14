b5abd6984a94d1852f9f7774f51d92abfdf1cba8
CaptStorm <cptstormbird@gmail.com>
2017-10-13 23:04:27 -0700

    Create dateObject.js

---
+++ dateObject.js
@@ -0,0 +1,12 @@
+var now = new Date();
+console.log(now.getFullYear());
+console.log(now.toString());
+
+var then = new Date('9/20/2018');
+console.log(then.toString());
+
+
+//output
+//2017
+//"Fri Oct 13 2017 22:59:33 GMT-0700 (Pacific Daylight Time)"
+//"Thu Sep 20 2018 00:00:00 GMT-0700 (Pacific Daylight Time)"
