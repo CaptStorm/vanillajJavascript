0e8c64564670188a96f8bb52ea6cb0ec3be90f2f
CaptStorm <cptstormbird@gmail.com>
2017-10-13 22:10:13 -0700

    Update return_a_function.js

--- return_a_function.js
+++ return_a_function.js
@@ -12,3 +12,25 @@ function doProcess(a, b) {
 
 var a = doProcess(89, 88);
 a();
+
+
+
+
+function doProcess(a, b) {
+    function displaySum() {
+        alert("the sum is " + (a + b));
+    }
+
+    function displayDiff() {
+        alert("the difference is " + (a - b));
+    }
+    function displayMult() {
+        alert("Multiply " + (a * b));
+    }
+    displaySum();
+    displayDiff();
+    return displayMult;
+}
+
+var function5 = doProcess(89, 88);
+function5();
