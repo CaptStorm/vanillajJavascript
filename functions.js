cc9f1514b02f543ff154b53d5acf86447b36e664
CaptStorm <cptstormbird@gmail.com>
2017-10-15 19:53:26 -0700

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,21 @@
+function myFunc(){
+  console.log("hi");
+}
+//call myFunc
+myFunc();
+
+
+
+function myFunc(a, b){
+  return a * b;
+}
+var fun = myFunc(8,9);
+console.log(fun);
+
+
+
+var add3  = function (a, b){
+  return a - b;
+}
+
+console.log(add3(5, 8));
