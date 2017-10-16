6859ee8d37c57b5ba47975c1518dbb0f4b0952c2
CaptStorm <cptstormbird@gmail.com>
2017-10-15 19:59:51 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -17,5 +17,21 @@ console.log(fun);
 var add3  = function (a, b){
   return a - b;
 }
-
 console.log(add3(5, 8));
+
+
+
+var x = 2;
+function numberLog (){
+  return x;
+}  
+console.log(numberLog());
+//output 2 
+
+var x = 2;
+function numberLog (){
+  var x = 34;
+  return x;
+}  
+console.log(numberLog());
+//output 34
