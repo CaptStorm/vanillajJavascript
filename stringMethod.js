28087e1911ad8649b2df631a305d0eefd8291d31
CaptStorm <cptstormbird@gmail.com>
2017-10-15 19:33:58 -0700

    Create stringMethod.js

---
+++ stringMethod.js
@@ -0,0 +1,8 @@
+var myString = "I love New York";
+console.log(myString.search ('ve'));
+
+
+console.log(myString.replace("New", "Old"));
+
+var splitMyString = myString.split("love");
+console.log(splitMyString);
