258f86b0d4a85d3e5350889a65fb6bddcff992b4
CaptStorm <cptstormbird@gmail.com>
2017-10-15 19:18:25 -0700

    Create MathObject.js

---
+++ MathObject.js
@@ -0,0 +1,14 @@
+var newMath = Math.PI;
+console.log(newMath);
+
+Math.abs();
+Math.sin();
+Math.pow();
+Math.random();
+Math.max();
+Math.min();
+Math.round();
+Math.max();
+Math.round();
+Math.floor();
+
