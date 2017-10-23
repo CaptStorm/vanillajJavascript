d5ad838eefcb6cc15e5a9eb32e4aaf9a62904815
CaptStorm <cptstormbird@gmail.com>
2017-10-23 01:55:28 -0700

    Create chars.js

---
+++ chars.js
@@ -0,0 +1,10 @@
+var s = "Hello";
+console.log("var S starts with " + s.charAt(0));
+
+
+var hello = "Hello from Google";
+console.log (hello.indexOf("f"));
+console.log (hello.slice(3, 9));
+console.log (hello.toUpperCase());
+console.log (hello.split("from"));
+console.log (hello.split("oo"));
