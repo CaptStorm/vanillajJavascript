d90b32c49c3d053e862d4d77ef5b6d84509cc4aa
CaptStorm <cptstormbird@gmail.com>
2017-10-23 13:22:24 -0700

    Update chars.js

--- chars.js
+++ chars.js
@@ -12,3 +12,24 @@ console.log (hello.split("oo"));
 var hello = "Hello from Google";
 console.log (hello.replace("Google", "Washington"));
 //output "Hello from Washington"
+
+
+
+var quote = "There is nothing either good or bad, but thinking makes it so.";
+var incQuote = quote.includes("or");
+console.log(incQuote);
+
+var searchQuote = quote.search("either");
+console.log(searchQuote);
+
+var replaceQuote = quote.replace("thinking", "everything");
+console.log(replaceQuote);
+
+var sliceQuote = quote.slice(0, 23);
+console.log(sliceQuote);
+
+var splitQuote = quote.split(" ");
+console.log(splitQuote);
+for (var i = 0; i<splitQuote.length; i++){
+  console.log(splitQuote[i] + "");
+};
