d593985d56bfcdbd22e84474aa93f62d648e9da9
CaptStorm <cptstormbird@gmail.com>
2017-10-23 14:26:00 -0700

    Update DateObject.js

--- DateObject.js
+++ DateObject.js
@@ -3,3 +3,7 @@ var then = new Date('2017, 10, 5');
 console.log(then.toUTCString());
 console.log(then.toString());
 console.log(then.getMonth());
+
+
+var date = new Date('6/29/2011 4:52:48 PM UTC');
+console.log(date.toString()); 
