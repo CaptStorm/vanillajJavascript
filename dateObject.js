7a18a9c08a2871e38c22f1f5f20e536d1617d5f9
CaptStorm <cptstormbird@gmail.com>
2017-10-23 14:04:25 -0700

    Update dateObject.js

--- dateObject.js
+++ dateObject.js
@@ -10,3 +10,17 @@ console.log(then.toString());
 //2017
 //"Fri Oct 13 2017 22:59:33 GMT-0700 (Pacific Daylight Time)"
 //"Thu Sep 20 2018 00:00:00 GMT-0700 (Pacific Daylight Time)"
+
+
+
+var now = new Date();
+
+
+var setDate = now.setFullYear(2100);
+  console.log(setDate);
+var setDate1 = now.setHours(20);
+  console.log(setDate1);
+var setDate2 = now.toString();
+  console.log(setDate2);
+var setDate3 = now.toLocaleDateString();
+  console.log(setDate3);
