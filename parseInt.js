9b4b0cbe42e21b1863d3bcffc8b7d30112a831ce
CaptStorm <cptstormbird@gmail.com>
2017-10-23 01:49:31 -0700

    Create parseInt.js

---
+++ parseInt.js
@@ -0,0 +1,13 @@
+var num = parseInt(342.4);
+console.log(num);
+
+var hexNum = parseInt('ff44', 16);
+console.log(hexNum);
+
+//parseFloat
+
+var floats = parseInt(9323.4);
+console.log(floats);
+
+var floats2 = parseInt(1.9999);
+console.log(floats2);
