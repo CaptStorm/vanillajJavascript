a599ef0a10fb7d9fde072f45129fb5e0188a78f2
CaptStorm <cptstormbird@gmail.com>
2017-10-23 00:22:40 -0700

    Create var and arrays.js

---
+++ var and arrays.js
@@ -0,0 +1,25 @@
+var car = "honda";
+
+//print to html page
+//document.write() is a function
+
+document.write(car);
+
+
+
+var x = 3;
+var y = 90;
+document.write(x+y);
+console.log(x+y);
+
+//array
+var cars = new Array ("Honda", "BMW", "Toyota", "Kia");
+document.write(cars[0]);
+document.write(cars);
+
+output: Honda,
+output for cars: Honda,BMW,Toyota,Kia
+
+document.write(cars.length); //4
+document.write(cars.indexOf("Toyota")); //2 because Honda is 0, BMW is 1, Toyota is 2
+
