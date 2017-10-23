6e00379e5b2998c8b5934305ff6f31883ed17432
CaptStorm <cptstormbird@gmail.com>
2017-10-23 02:20:14 -0700

    Create manupulatingArray.js

---
+++ manupulatingArray.js
@@ -0,0 +1,16 @@
+
+var names = ["John", "Peter", "Alex", "Eve", "James"];
+
+
+console.log(names.sort());
+console.log(names[2]);
+console.log(names.push("Anna"));
+console.log(names.sort(names));
+console.log(names.unshift("Elizabeth"));
+console.log(names.sort(names));
+console.log(names.push("Victoria"));
+console.log(names.length);
+console.log(names.pop("Peter"));
+console.log(names.sort(names));
+console.log(names.splice("Eve"));
+console.log(names.sort(names));
